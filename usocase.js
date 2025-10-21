450011b17489ba810a0ff5d975af7086df1860b8
SusanaUmg2023 <148268120+SusanaUmg2023@users.noreply.github.com>
2025-10-20 21:52:15 -0600

    Add files via upload

---
+++ usocase.js
@@ -0,0 +1,20 @@
+let col;
+document.write('Ejemplo de case');
+document.write('<br>');
+
+
+        col = prompt('Ingrese alguno de estos tres colores (rojo, verde, azul)');
+        switch (col) {
+            case 'rojo':
+                document.write('Se ingresó rojo');
+                break;
+            case 'verde':
+                document.write('Se ingresó verde');
+                break;
+            case 'azul':
+                document.write('Se ingresó azul');
+                break;
+        }
+document.write('<br>');
+
+
